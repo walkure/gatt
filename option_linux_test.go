5e875372eb38a173430b5cfa2678e2480b0e85bd
Michel Wohlert <michel@1012consulting.io>
2021-04-21 22:06:28 -0700

    Add LnxSetScanParameters to allow setting scan parameters of device

--- option_linux_test.go
+++ option_linux_test.go
@@ -54,6 +54,19 @@ func ExampleLnxSetAdvertisingParameters() {
 	d.Option(o)          // Or dynamically with Option.
 }
 
+func ExampleLnxSetScanParameters() {
+	o := LnxSetScanParameters(&cmd.LESetScanParameters{
+		LEScanType:           0x01,   // [0x00]: passive, 0x01: active
+		LEScanInterval:       0x0010, // [0x10]: 0.625ms * 16
+		LEScanWindow:         0x0010, // [0x10]: 0.625ms * 16
+		OwnAddressType:       0x00,   // [0x00]: public, 0x01: random
+		ScanningFilterPolicy: 0x00,   // [0x00]: accept all, 0x01: ignore non-white-listed.
+	})
+
+	d, _ := NewDevice(o) // Can be used with NewDevice.
+	d.Option(o)          // Or dynamically with Option.
+}
+
 func ExampleLnxSendHCIRawCommand_predefinedCommand() {
 	// Send a predefined command of cmd package.
 	c := &cmd.LESetScanResponseData{
